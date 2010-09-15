3b24bd6c09c0ca1ef82ec1b97f06475c0e4bab34
Susanne Gottwald <gottwald@zib.de>
2010-09-15 12:30:04 +0000

    erster selenium test fürs publish modul
    git-svn-id: https://svn.zib.de/opus4dev/selenium@5421 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/publish/testDepositInstitute_Test.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+class Example extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl("http://opus4web.zib.de/");
+  }
+
+  public function testDepositInstitute()
+  {
+    $this->open("/opus4-devel/publish");
+    $this->click("send");
+    $this->type("PersonAuthor1FirstName", "Susanne");
+    $this->type("PersonAuthor1LastName", "Gottwald");
+    $this->select("Institute1", "label=Zuse Institute Berlin (ZIB)");
+    $this->select("Language", "label=English");
+    $this->type("TitleMain1", "Entenhausen");
+    $this->select("TitleMain1Language", "label=English");
+    $this->select("Project1", "label=A12");
+    $this->type("SubjectMSC1", "00A09");
+    $this->click("send");
+    $this->type("SubjectMSC1", "00A0");
+    $this->click("send");
+  }
+}
+?>
