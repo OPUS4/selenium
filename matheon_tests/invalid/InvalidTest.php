746816269da776db5cc8ee5412455fa3a708dcff
Sascha Szott <szott@gmx.de>
2010-09-17 08:51:59 +0000

    check screenshot capability on matheon instance
    git-svn-id: https://svn.zib.de/opus4dev/selenium@5514 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ matheon_tests/invalid/InvalidTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require_once 'PHPUnit/Extensions/SeleniumTestCase.php';
+
+class InvalidTest extends PHPUnit_Extensions_SeleniumTestCase
+{
+  protected $captureScreenshotOnFailure = TRUE;
+  protected $screenshotPath = '/home/opus4ci/cruisecontrol/webapps/screenshots';
+  protected $screenshotUrl = 'http://opus4ci.zib.de:8080/screenshots';
+ 
+  protected function setUp()
+  {
+    $this->setBrowser("*firefox");
+    $this->setBrowserUrl("http://opus4web.zib.de");
+  }
+
+  public function testOpus5()
+  {
+    //$this->markTestSkipped('only used for demonstration purpose');
+    $this->open("/opus4-matheon/home");
+    $this->assertTrue($this->isTextPresent("Opus 5"));
+  }
+}
+?>
