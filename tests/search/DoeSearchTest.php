6fef7f436964d32759c8b4c42bdcd4a4e29966de
Sascha Szott <szott@gmx.de>
2010-09-28 14:41:07 +0000

    sasz: fixed DoeSearchTest
    git-svn-id: https://svn.zib.de/opus4dev/selenium@5968 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/search/DoeSearchTest.php
+++ tests/search/DoeSearchTest.php
@@ -7,25 +7,12 @@ class DoeSearchTest extends TestCase
 
   public function testDoeSearch()
   {
-    $this->open("/opus4-selenium/home");
-    try {
-        $this->assertTrue($this->isTextPresent("Opus 4"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
-    }
+    $this->open("/opus4-selenium/solrsearch");
     $this->type("query", "doe");
     $this->click("//input[@value='Search']");
     $this->waitForPageToLoad("30000");
-    try {
-        $this->assertTrue($this->isTextPresent("5 Search Results"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
-    }
-    try {
-        $this->assertTrue($this->isTextPresent("Display of results 1 to 5"));
-    } catch (PHPUnit_Framework_AssertionFailedError $e) {
-        array_push($this->verificationErrors, $e->toString());
-    }
+    $this->assertTrue($this->isTextPresent("5 Search Results"));
+    $this->assertTrue($this->isTextPresent("Display of results 1 to 5"));
   }
 }
 ?>
