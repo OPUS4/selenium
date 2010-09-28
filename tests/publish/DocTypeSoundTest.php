5e709752cfd9b3d314af59563327a488f8cafbf3
Sascha Szott <szott@gmx.de>
2010-09-28 15:43:32 +0000

    sasz: added new DocTypeSoundTest
    git-svn-id: https://svn.zib.de/opus4dev/selenium@5973 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/publish/DocTypeSoundTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'TestCase.php';
+
+class TonTest extends TestCase
+{
+  
+  public function testDocTypeTon()
+  {
+    $this->open("/opus4-selenium/publish");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isTextPresent("Publish"));
+    $this->select("documentType", "label=Sound");
+    $this->click("send");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isTextPresent("Sound"));
+    $this->type("PersonSubmitterFirstName1", "Donald");
+    $this->type("PersonSubmitterLastName1", "Trump");
+    $this->type("TitleMain1", "Millionär gesucht");
+    $this->select("TitleMainLanguage1", "label=German");
+    $this->type("PersonAuthorFirstName1", "Donald");
+    $this->type("PersonAuthorLastName1", "Trump");
+    $this->type("CompletedDate", "12.03.2004");
+    $this->select("Language", "label=German");
+    $this->select("Licence", "label=Creative Commons - Namensnennung");
+    $this->click("send");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isTextPresent("Please check your data."));
+    $this->click("send");
+    $this->waitForPageToLoad("30000");
+    $this->assertTrue($this->isTextPresent("Publishing document successful"));
+  }
+}
+?>
