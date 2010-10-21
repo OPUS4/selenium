33498140f6e045cccd56b161fa16d2b6ab931f41
Susanne Gottwald <gottwald@zib.de>
2010-10-21 13:02:09 +0000

    changes for required file upload
    git-svn-id: https://svn.zib.de/opus4dev/selenium@6578 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- matheon_tests/publish/DepositInstituteTest.php
+++ matheon_tests/publish/DepositInstituteTest.php
@@ -11,8 +11,7 @@ class DepositInstituteTest extends TestCase
     $this->click("link=Publish");
     $this->waitForPageToLoad("30000");
     $this->click("send");
-    $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isTextPresent("Value is required and can't be empty."));
+    $this->waitForPageToLoad("30000");    
     $this->assertTrue($this->isTextPresent("Please upload at least one file."));
     $this->select("documentType", "label=preprintmatheon");
     $this->type("fileupload-0", "D:\\Meine Daten\\Dokumente\\Info Vermoegen.pdf");
