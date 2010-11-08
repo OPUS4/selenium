a98b6ec6caefe591a323b77e86216773b7345b90
Sascha Szott <szott@gmx.de>
2010-11-08 16:10:00 +0000

    sasz: updated assertions in DoeSearchTest after renaming some entries
    git-svn-id: https://svn.zib.de/opus4dev/selenium@6914 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/search/DoeSearchTest.php
+++ tests/search/DoeSearchTest.php
@@ -11,8 +11,8 @@ class DoeSearchTest extends TestCase
     $this->type("query", "doe");
     $this->click("//input[@value='Search']");
     $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isTextPresent("5 Search Results"));
-    $this->assertTrue($this->isTextPresent("Display of results 1 to 5"));
+    $this->assertTrue($this->isTextPresent("5 search hits"));
+    $this->assertTrue($this->isTextPresent("1 to 5"));
   }
 }
 ?>
