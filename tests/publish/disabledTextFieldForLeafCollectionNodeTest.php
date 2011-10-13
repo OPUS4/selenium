3c64fecdd35f7e384286fb9391dc11f3ccf3ceec
Susanne Gottwald <gottwald@zib.de>
2011-10-13 13:37:56 +0000

    IMPROVEMENT: 2 Regression-Tests: a) test if there is a disabled text field when collection leaf is reached b) test if there is a disabled text field for series and browse buttons Issue #OPUSVIER-1449 - Unzureichende Validierung von Formularwerten
    git-svn-id: https://svn.zib.de/opus4dev/selenium@9116 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/publish/disabledTextFieldForLeafCollectionNodeTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Module_Publish
+ * @package     Module_Publish Selenium Test MATHEON
+ * @author      Susanne Gottwald <gottwald@zib.de>
+ * @copyright   Copyright (c) 2008-2011, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+require_once 'TestCase.php';
+
+class disabledTextFieldForLeafCollectionNodeTest extends TestCase {
+
+    public function testdisabledTextFieldForLeafCollectionNode() {
+        $this->open("/opus4-selenium");
+        $this->waitForPageToLoad("30000");
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad("30000");
+        $this->open("/opus4-selenium/publish");
+        $this->click("//li[@id='primary-nav-publish']/a/em/span");
+        $this->waitForPageToLoad("30000");
+
+        $this->select("id=documentType", "label=Preprint für MATHEON");
+        $this->click("rights");
+        $this->click("send");
+        $this->waitForPageToLoad("30000");
+        $this->type("PersonAuthorFirstName1", "Susi");
+        $this->type("PersonAuthorLastName1", "Gottwald");
+        $this->type("TitleMain1", "Entenhausen");
+        $this->type("TitleAbstract1", "bla");
+        $this->select("SubjectMSC1", "label=00-XX GENERAL");
+        $this->click("browseDownSubjectMSC");
+        $this->waitForPageToLoad("30000");
+        $this->click("browseDownSubjectMSC");
+        $this->waitForPageToLoad("30000");
+        $this->assertTrue($this->isTextPresent("Sie haben das Ende dieser Sammlung erreicht."));
+        $this->assertTrue($this->isEditable("collId2SubjectMSC1"));
+        $this->assertFalse($this->isElementPresent("collId3SubjectMSC1"));
+    }
+
+}
+
+?>
