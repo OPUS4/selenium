3adc89860b6e064da1dbd6c167160907b88a7e23
Sascha Szott <szott@gmx.de>
2012-01-03 18:14:30 +0000

    fixed test after adding new test document written by John Doe
    git-svn-id: https://svn.zib.de/opus4dev/selenium@9596 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/search/DoeSearchTest.php
+++ tests/search/DoeSearchTest.php
@@ -11,8 +11,8 @@ class DoeSearchTest extends TestCase
     $this->type("query", "doe");
     $this->click("//input[@value='Search']");
     $this->waitForPageToLoad("30000");
-    $this->assertTrue($this->isTextPresent("5 search hits"));
-    $this->assertTrue($this->isTextPresent("1 to 5"));
+    $this->assertTrue($this->isTextPresent("6 search hits"));
+    $this->assertTrue($this->isTextPresent("1 to 6"));
   }
 }
 ?>
