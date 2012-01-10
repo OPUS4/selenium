c3a49abfedd2e0eecd2fb802ebc7d982c5c8f2b2
Jens Schwidder <schwidder@zib.de>
2012-01-10 12:22:14 +0000

    Funktion zum Einloggen zur Basisklasse für Selenium-Tests hinzugefügt. Issue #OPUSVIER-2172 - Collection-Edit-Formular zeigt Wert 0 für Attribut oai_subset und number nicht an
    git-svn-id: https://svn.zib.de/opus4dev/selenium@9717 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,6 +13,18 @@ class TestCase extends PHPUnit_Extensions_SeleniumTestCase
     $this->setBrowserUrl("http://opus4web.zib.de");
   }
 
+    protected function login($user = 'admin', $password = 'adminadmin') {
+        // make sure logged out
+        $this->open('/opus4-selenium/auth/logout');
+        // login
+        $this->open('/opus4-selenium/auth/login');
+        $this->waitForPageToLoad('30000');
+        $this->type('login', 'admin');
+        $this->type('password', 'adminadmin');
+        $this->click('SubmitCredentials');
+        $this->waitForPageToLoad('30000');
+    }
+
 }
 
 ?>
