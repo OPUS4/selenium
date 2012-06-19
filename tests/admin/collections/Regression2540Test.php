d894daff9c42f4e236f4626309d0901c6de61fcf
Sascha Szott <szott@gmx.de>
2012-06-19 17:42:57 +0000

    added regression test for OPUSVIER-2540
    git-svn-id: https://svn.zib.de/opus4dev/selenium@10534 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/admin/collections/Regression2540Test.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Selenium Test
+ * @package     Module_Admin
+ * @author      Sascha Szott <szott@zib.de>
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+require_once 'TestCase.php';
+
+class Regression2540Test extends TestCase {
+
+    public function testCreateCollectionRole() {
+        $this->login();
+
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad();
+
+        $this->open("/opus4-selenium/admin/collectionroles/new");
+        $this->waitForPageToLoad();
+
+        $this->type("id=Opus_Model_Filter-Name-1", "foobar");
+        $this->type("id=Opus_Model_Filter-OaiName-1", "foobar");
+        $this->click("id=submit");
+
+        $this->assertTextNotPresent("Collection role 'foobar' successfully created.");
+        $this->assertTextPresent("Sammlung 'foo' wurde erfolgreich angelegt.");
+    }
+
+    public function testCreateCollection() {
+        $this->login();
+
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad();
+
+        // TODO do not hard-code collection id
+        $this->open("/opus4-selenium/admin/collection/new/id/16202/type/child");
+        $this->waitForPageToLoad();
+
+        $this->type("id=Opus_Model_Filter-Name-1", "collfoobar");
+        $this->type("id=Opus_Model_Filter-Number-1", "12345");
+        $this->type("id=Opus_Model_Filter-OaiSubset-1", "collfoobar");
+        $this->click("id=submit");
+
+        $this->assertTextNotPresent('Insert successful');
+        $this->assertTextPresent("Sammlungseintrag 'collfoobar' wurde erfolgreich angelegt.");
+    }
+
+    public function testEditCollectionRole() {
+        $this->login();
+
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad();
+
+        $this->open("/opus4-selenium/admin/collectionroles/edit/roleid/18");
+        $this->waitForPageToLoad();
+
+        $this->click("id=submit");
+
+        $this->assertTextNotPresent("Collection role 'foobar' successfully edited.");
+        $this->assertTextPresent("Sammlung 'foo' wurde erfolgreich bearbeitet.");
+    }
+
+    public function testEditCollection() {
+        $this->login();
+
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad();
+
+        // TODO do not hard-code collection id
+        $this->open("/opus4-selenium/admin/collection/edit/id/16203");
+        $this->waitForPageToLoad();
+
+        $this->click("id=submit");
+
+        $this->assertTextNotPresent("Edit successful");
+        $this->assertTextPresent("Sammlungseintrag 'collfoobar' wurde erfolgreich bearbeitet.");
+    }
+
+    public function testDeleteCollection() {
+        $this->login();
+
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad();
+
+        // TODO do not hard-code collection id
+        $this->open("/opus4-selenium/admin/collection/delete/id/16203");
+        $this->waitForPageToLoad();
+
+        $this->assertTextNotPresent('Operation completed successfully.');
+        $this->assertTextPresent("Sammlungseintrag 'collfoobar' wurde erfolgreich gelöscht.");
+    }
+
+    public function testDeleteCollectionRole() {
+        $this->login();
+
+        $this->open("/opus4-selenium/home/index/language/language/de");
+        $this->waitForPageToLoad();
+
+        // TODO do not hard-code collection id
+        $this->open("/opus4-selenium/admin/collectionroles/delete/roleid/18");
+        $this->waitForPageToLoad();
+
+        $this->assertTextNotPresent('Operation completed successfully.');
+        $this->assertTextPresent("Sammlung 'foo' wurde erfolgreich gelöscht.");
+    }
+
+}
