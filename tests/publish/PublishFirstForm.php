OPUS4/application

commit 4fc656a18333119e2af78c6fe621a8a1b7c4bd9f

do not translate twice in first step of publish form Issue #OPUSVIER-2645 - erste Seite des Publikationsformulars wird doppelt durch den Translate-Mechanismus geschleift
git-svn-id: https://svn.zib.de/opus4dev/server/trunk@10823 0a49f362-4213-0410-86e3-e96cb6c4fbae

modules/publish/controllers/IndexController.php
@@ -48,32 +48,27 @@ class Publish_IndexController extends Controller_Action {
         //unset all possible session content
         $session->unsetAll();
 
-        $this->view->title = $this->view->translate('publish_controller_index');        
-
+        $this->view->title = 'publish_controller_index';
+        
         $form = new Publish_Form_PublishingFirst();
 
-        //set action_url and give it to the view
-        $action_url = $this->view->url(array('controller' => 'form', 'action' => 'upload'));
-        $form->setAction($action_url);
-        $form->setMethod('post');
-        $this->view->action_url = $action_url;        
-        $this->view->form = $form;
+        $this->view->action_url = $this->view->url(array('controller' => 'form', 'action' => 'upload'));
         $this->view->showBib = $form->bibliographie;
         $this->view->showRights = $form->showRights;
         $this->view->enableUpload = $form->enableUpload;
-
         if (!$form->enableUpload) {
-            $this->view->subtitle = $this->view->translate('publish_controller_index_sub_without_file');
+            $this->view->subtitle = 'publish_controller_index_sub_without_file';
         }
         else {
-            $this->view->subtitle = $this->view->translate('publish_controller_index_sub');
+            $this->view->subtitle = 'publish_controller_index_sub';
         }
 
         //initialize session variables
+        // TODO hide initialization routine
         $session->documentType = "";
         $session->documentId = "";
         $session->disabled = array();
         $session->endOfCollectionTree = array();        
-        $session->additionalFields = array();
+        $session->additionalFields = array();        
     }
 }

modules/publish/forms/PublishingAbstract.php
@@ -36,12 +36,13 @@ abstract class Publish_Form_PublishingAbstract extends Zend_Form {
 
     protected $config;
     protected $session;
+    protected $view;
 
     public function __construct() {
         $this->session = new Zend_Session_Namespace('Publish');
         $this->config = Zend_Registry::get('Zend_Config');
-
-        parent::__construct();
+        $this->view = $this->getView();
+        parent::__construct();        
     }
 
     function getElementAttributes($elementName) {
@@ -134,7 +135,8 @@ abstract class Publish_Form_PublishingAbstract extends Zend_Form {
      */
     function addSubmitButton($label, $name) {
         $submit = $this->createElement('submit', $name);
-        $submit->setLabel($label);
+        $submit->setDisableTranslator(true);
+        $submit->setLabel($this->view->translate($label));
         $this->addElement($submit);
         return $submit;
     }

modules/publish/forms/PublishingFirst.php
@@ -39,7 +39,6 @@
  */
 class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
 
-    public $view;
     public $bibliographie;
     public $showRights;
     /**
@@ -48,13 +47,7 @@ class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
      */
     public $enableUpload = false;
 
-    /**
-     *
-     * @param <type> $view View Object from Controller
-     * @param <type> $options
-     */
-    public function __construct() {
-        $this->view = $this->getView();        
+    public function __construct() {        
         parent::__construct();
     }
 
@@ -82,30 +75,34 @@ class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
      */
     public function init() {
         parent::init();
+
+        $this->setDisableTranslator(true);
         
         //create and add document type
         $doctypes = $this->_createDocumentTypeField();
-        $this->addElement($doctypes);
+        $this->addElement($doctypes);        
 
         //create and add file upload
         $this->enableUpload = ($this->config->form->first->enable_upload == 1);
         if ($this->enableUpload) {
-            $fileupload = $this->_createFileuploadField();
-            $this->addDisplayGroup($fileupload, 'documentUpload');
+            $fileupload = $this->_createFileuploadField();            
+            $this->addDisplayGroup($fileupload, 'documentUpload');            
         }
 
         //create and add bibliographie
         $bibliographie = $this->_createBibliographyField();
-        if (!is_null($bibliographie))
-            $this->addElement($bibliographie);
+        if (!is_null($bibliographie)) {
+            $this->addElement($bibliographie);            
+        }
 
         //create and add rights checkbox
         $rights = $this->_createRightsCheckBox();
-        if (!is_null($rights))
+        if (!is_null($rights)) {
             $this->addElement($rights);
+        }
 
-        //create and add send-button
-        $this->addSubmitButton('Send', 'send');
+        // TODO can be removed?
+        //$this->addSubmitButton('Send', 'send');
         
         $this->setAttrib('enctype', Zend_Form::ENCTYPE_MULTIPART);
         $this->setViewValues();
@@ -135,7 +132,8 @@ class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
         $doctypes = $this->createElement('select', 'documentType');
         $doctypes->setLabel('selecttype')
                 ->setMultiOptions(array_merge(array('' => 'choose_valid_doctype'), $translatedOptions))
-                ->setRequired(true);
+                ->setRequired(true)
+                ->setDisableTranslator(true);
 
         return $doctypes;
     }
@@ -177,19 +175,22 @@ class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
                 ->setAttrib('enctype', 'multipart/form-data');
 
         if (1 == $requireUpload) {
-            if (!isset($this->session->fulltext) || $this->session->fulltext == '0')
+            if (!isset($this->session->fulltext) || $this->session->fulltext == '0') {
                 $fileupload->setRequired(true);
+            }
         }
-        else
+        else {
             $fileupload->setRequired(false);
+        }
 
+        $fileupload->setDisableTranslator(true);
         $this->addElement($fileupload);
 
-        //create add-button        
         $this->addSubmitButton('addAnotherFile', 'addAnotherFile');
         
         $comment = $this->createElement('textarea', 'uploadComment');
         $comment->setLabel('uploadComment');
+        $comment->setDisableTranslator(true);
         $this->addElement($comment);
 
         $group = array($fileupload->getName(), 'addAnotherFile', $comment->getName());
@@ -214,6 +215,7 @@ class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
             $this->bibliographie = 1;
             $bibliographie = $this->createElement('checkbox', 'bibliographie');
             $bibliographie->setLabel('bibliographie');
+            $bibliographie->setDisableTranslator(true);
         }
 
         return $bibliographie;
@@ -233,7 +235,8 @@ class Publish_Form_PublishingFirst extends Publish_Form_PublishingAbstract {
             $rightsCheckbox = $this->createElement('checkbox', 'rights')
                     ->setLabel('rights')
                     ->setRequired(true)
-                    ->setChecked(false);
+                    ->setChecked(false)
+                    ->setDisableTranslator(true);
         }
 
         return $rightsCheckbox;

modules/publish/forms/PublishingSecond.php
@@ -49,13 +49,10 @@ class Publish_Form_PublishingSecond extends Publish_Form_PublishingAbstract {
     public $additionalFields = array();
     public $postData = array();
     public $log;
-    public $view;
 
     public function __construct($log, $postData = null) {
         $this->postData = $postData;
         $this->log = $log;
-        $this->view = $this->getView();
-
         parent::__construct();
     }
 

modules/publish/views/helpers/Element.php
@@ -59,8 +59,8 @@ class Publish_View_Helper_Element extends Publish_View_Helper_Fieldset {
     function _renderElement($field, $options=null, $name=null) {
         
         $fieldset = "<fieldset class='left-labels'>";
-        $fieldset .= $this->getLegendFor($field['header']);        
-        $fieldset .= $this->getFieldsetHint($field['id']);        
+        $fieldset .= $this->getLegendFor($field['header']);
+        $fieldset .= $this->getFieldsetHint($field['id']);
         $fieldset .= "<div class='form-item'>";        
         $fieldset .= $this->getLabelFor($field['id'], $field['label'], $field['req']);
         

modules/publish/views/helpers/Fieldset.php
@@ -45,31 +45,36 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
         $fieldset = "";
         if (!isset($field['isLeaf'])) {
             $fieldset .= "\n\t\t\t\t<input type='text' class='form-textfield' name='" . $field['id'] . "' id='" . $field['id'] . "' ";
-            if ($options !== null)
+            if ($options !== null) {
                 $fieldset .= $options . " ";
-            else
+            }
+            else {
                 $fieldset .= "size='30' ";
+            }
 
             if ($field['disabled'] === true) {
                 $fieldset .= " disabled='1' ";
                 $this->disable = true;
             }
-           
-            $fieldset .= " title='" . htmlspecialchars($this->view->translate($field['hint']), ENT_QUOTES) . "' ";
 
+            $fieldset .= " title='" . htmlspecialchars($this->view->translate($field['hint']), ENT_QUOTES) . "' ";
+            
             $fieldset .= " value='" . htmlspecialchars($field['value'], ENT_QUOTES) . "' />\n";            
-            if (isset($field['desc']))
+            if (isset($field['desc'])) {
                 $fieldset .= '<div class="description hint">' . $this->view->translate($field['desc']) . '</div>';
+            }
         }
         return $fieldset;
     }
 
     function renderHtmlTextarea($field, $options) {
         $fieldset = "\n\t\t\t\t<textarea name='" . $field['id'] . "' class='form-textarea' ";
-        if ($options !== null)
+        if ($options !== null) {
             $fieldset .= $options . " ";
-        else
+        }
+        else {
             $fieldset .= "cols='30' rows='5' ";
+        }
 
         if ($field['disabled'] === true) {
             $fieldset .= " disabled='1' ";
@@ -77,7 +82,6 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
         }
 
         $fieldset .= " title='" . htmlspecialchars($this->view->translate($field['hint']), ENT_QUOTES) . "' ";
-
         $fieldset .= " id='" . $field['id'] . "'>" . htmlspecialchars($field['value'], ENT_QUOTES) . "</textarea>";
 
         return $fieldset;
@@ -86,7 +90,7 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
     // TODO: wofür wird der Parameter $options benötigt?
     function renderHtmlSelect($field, $options) {
         $fieldset = "\n\t\t\t\t" . '<select style="width:300px" name="' . $field['id'] . '" class="form-selectfield"  id="' . $field['id'] . '"';
-        $fieldset .= ' title="' . htmlspecialchars ($this->view->translate($field['hint']), ENT_QUOTES) . '"';        
+        $fieldset .= ' title="' . htmlspecialchars ($this->view->translate($field['hint']), ENT_QUOTES) . '"';
         if ($field['disabled'] === true) {
             $fieldset .= ' disabled="1" ';
         }
@@ -110,6 +114,7 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
         return $fieldset;
     }
 
+    // TODO: wofür wird der Parameter $options benötigt?
     function renderHtmlCheckbox($field, $options) {
         $fieldset = "<input type='hidden' name='" . $field['id'] . "' value='0' />";
         $fieldset .= "\n\t\t\t\t<input type='checkbox' class='form-checkbox' name='" . $field['id'] . "' id='" . $field['id'] . "' ";
@@ -130,10 +135,12 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
     function renderHtmlFile($field, $options) {
         $fieldset = "<input type='file' name='" . $field['id'] . "' id='" . $field['id'] . "' enctype='multipart/form-data' ";
         $fieldset .= "title='" . htmlspecialchars($this->view->translate($field['hint']), ENT_QUOTES) . "' ";
-        if ($options !== null)
+        if ($options !== null) {
             $fieldset .= $options . " ";
-        else
+        }
+        else {
             $fieldset .= "size='30'";
+        }
         $fieldset .= " />\n";
 
         return $fieldset;
@@ -195,11 +202,13 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
     
     function _renderSubmit($value, $options=null, $name=null) {
         $submit = "\n\t\t<input type='submit' name='" . $name . "' id='" . $name . "' value='" . htmlspecialchars($this->view->translate($value), ENT_QUOTES) . "' ";
-        if (isset($options))
+        if (isset($options)) {
             $submit .= $options . " />";
+        }
         return $submit;
     }
-    
+
+    // TODO: wofür wird der Parameter $options benötigt?
     function _renderHidden($value, $options = null, $name=null) {
         $hiddenfield = "<input type='hidden' name='" . $name . "' id='" . $name . "' value='" . $value . "' />";
         return $hiddenfield;
@@ -218,18 +227,18 @@ class Publish_View_Helper_Fieldset extends Zend_View_Helper_Abstract {
      * @param <String> Name of element or group
      * @return <type>
      */
-    function getFieldsetHint($name) {        
+    function getFieldsetHint($name) {
         return "<div class='description hint'><p>" . $this->view->translate('hint_' . $name) . "</div></p>";
     }
 
     function getLabelFor($name, $label, $required) {
         $fieldset = "<label for='" . $name . "'>" . htmlspecialchars($this->view->translate($label), ENT_QUOTES);
 
-        if ($required === 'required')
+        if ($required === 'required') {
             $fieldset .= $this->getRequiredSign();
+        }
 
         $fieldset .= "</label>";
-
         return $fieldset;
     }
 

modules/publish/views/helpers/Group.php
@@ -42,7 +42,7 @@ class Publish_View_Helper_Group extends Publish_View_Helper_Fieldset {
      * @param <type> $name name of possible hidden element
      * @return element to render in view
      */
-    public function group($value, $options = null, $name = null) {        
+    public function group($value, $options = null, $name = null) {
         $this->view->count++;
         if ($name == null && $value == null) {
             $error_message = $this->view->translate('template_error_unknown_field');

modules/publish/views/scripts/index/index.phtml
@@ -31,17 +31,11 @@
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
  * @version     $Id$
  */
-//for printing a generized Zend Form with standard layout:
-//echo $this->form;
-//element($value, $options = null, $type = null, $name = null)
-//group($value, $options = null, $name = null)
-//echo $this->form;
-
 ?>
 
-<h2><?= $this->title ?></h2>
+<h2><?= $this->translate($this->title) ?></h2>
 <div class="content">
-    <h3 class="document-type"><?= $this->subtitle ?></h3>
+    <h3 class="document-type"><?= $this->translate($this->subtitle) ?></h3>
 
     <?php if (isset($this->errorCaseMessage)) : ?>
         <div class="form-hint form-errors"><p><?= $this->errorCaseMessage; ?></p></div>
@@ -68,6 +62,6 @@
             <div class="button-wrapper">
                 <?= $this->element('button_label_send', "class='form-button submit-button'", "Submit", "send"); ?>
             </div>
-        </div>
+        </div>            
     </form>
 </div>

OPUS4/selenium

commit ef683a1ef7c0f96254b09fd1faa56a7d6a24177e

added selenium test to detect bug on first form step that was introduced in r10823 or r10826 Issue #OPUSVIER-2645 - erste Seite des Publikationsformulars wird doppelt durch den Translate-Mechanismus geschleift
git-svn-id: https://svn.zib.de/opus4dev/selenium@10829 0a49f362-4213-0410-86e3-e96cb6c4fbae

tests/publish/PublishFirstForm.php
@@ -1,0 +1,99 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Selenium Test
+ * @author      Sascha Szott <szott@zib.de>
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+require_once 'TestCase.php';
+
+class PublishFirstForm extends TestCase {
+
+    public function testMissingRightsInFirstFormStep() {
+        $this->switchToGerman();
+
+        $this->open("/opus4-selenium/publish");
+        $this->waitForPageToLoad();
+
+        $this->select("documentType", "value=all");
+        $this->click("send");
+        $this->waitForPageToLoad();
+       
+        $this->assertTrue($this->isTextPresent("Fehler: Bestätigen Sie bitte unsere rechtlichen Hinweise."));
+
+        $this->click('rights');
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Alle Felder (Testdokumenttyp)"));
+        $this->assertFalse($this->isTextPresent("Dokumenttyp und Datei wählen"));
+    }
+
+    public function testMissingDoctypeInFirstFormStep() {
+        $this->switchToGerman();
+
+        $this->open("/opus4-selenium/publish");
+        $this->waitForPageToLoad();
+
+        $this->click('rights');
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Fehler: Dokumenttyp fehlt!"));
+
+        $this->select("documentType", "value=all");
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Alle Felder (Testdokumenttyp)"));
+        $this->assertFalse($this->isTextPresent("Dokumenttyp und Datei wählen"));
+    }
+
+    public function testMissingRightsAndDoctypeInFirstFormStep() {
+        $this->switchToGerman();
+
+        $this->open("/opus4-selenium/publish");
+        $this->waitForPageToLoad();
+        
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Fehler: Dokumenttyp fehlt!"));
+        $this->assertTrue($this->isTextPresent("Fehler: Bestätigen Sie bitte unsere rechtlichen Hinweise."));
+
+        $this->select("documentType", "value=all");
+        $this->click('rights');
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Alle Felder (Testdokumenttyp)"));
+        $this->assertFalse($this->isTextPresent("Dokumenttyp und Datei wählen"));
+    }
+
+}