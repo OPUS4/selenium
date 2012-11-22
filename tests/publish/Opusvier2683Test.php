bb39c5db7245f357823eb696adff5d95f36a95a4
Sascha Szott <szott@gmx.de>
2012-11-22 17:41:24 +0000

    added regression test for OPUSVIER-2683 Issue #OPUSVIER-2683 - Übersetzungsschlüssel der Hints für Browsing-Felder im zweiten Publikationsformular dürfen keine fortlaufende Nummer bekommen
    git-svn-id: https://svn.zib.de/opus4dev/selenium@10850 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/publish/Opusvier2683Test.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Application
+ * @package     Selenium Test
+ * @author      Sascha Szott <szott@zib.de>
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+require_once 'TestCase.php';
+
+class Opusvier2683Test extends TestCase {
+
+    /**
+     * Regression Test for OPUSVIER-2683
+     */
+    public function testClassificationHintsTranslated() {
+        $this->switchToGerman();
+
+        $this->open("/opus4-selenium/publish");
+        $this->waitForPageToLoad();
+
+        $this->select("documentType", "value=all");
+        $this->click('rights');
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Tragen Sie bitte eine JEL Klasse ein."));
+        $this->select("id=SubjectJEL_1", "label=A General Economics and Teaching");
+        $this->click("id=browseDownSubjectJEL");
+        $this->waitForPageToLoad();
+
+        $this->assertFalse($this->isTextPresent("hint_collId2SubjectJEL"));
+        $this->select("id=collId2SubjectJEL_1", "label=A3 Collective Works");
+        $this->click("id=browseDownSubjectJEL");
+        $this->waitForPageToLoad();
+
+        $this->assertFalse($this->isTextPresent("hint_collId3SubjectJEL"));
+        $this->click("LegalNotices");
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Bitte überprüfen Sie Ihre Eingaben."));
+        $this->assertTrue($this->isTextPresent("JEL Sachgruppen / Klassifikationen"));
+        $this->assertTrue($this->isTextPresent("Sammlungseintrag"));
+        $this->assertTrue($this->isTextPresent("A30 General"));
+
+        $this->click("send");
+        $this->waitForPageToLoad();
+
+        $this->assertTrue($this->isTextPresent("Möchten Sie ein weiteres Dokument veröffentlichen?"));
+    }
+}
