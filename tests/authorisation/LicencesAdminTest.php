84061f14fd1d9d546cf65ec301f6fd5435accb89
Jens Schwidder <schwidder@zib.de>
2012-12-11 09:47:23 +0000

    Erste einfache Selenium Tests für Fall "Lizenz Administrator" hinzugefügt. Issue #OPUSVIER-2710 - Tests für erweiterte Recht im Admin Modul hinzufügen
    git-svn-id: https://svn.zib.de/opus4dev/selenium@10966 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/authorisation/LicencesAdminTest.php
@@ -0,0 +1,87 @@
+<?php
+
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Selenium Tests 
+ * @package     Authorisation
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+require_once 'authorisation/TestCaseAuthorisation.php';
+
+/**
+ * 
+ */
+class AuthorisationTest extends TestCaseAuthorisation {
+    
+    /**
+     * Prüft, ob nur die erlaubten Einträge im Admin Menu angezeigt werden.
+     */
+    public function testAdminMenuFiltering() {
+        $this->switchToEnglish();
+        $this->login("security2", "security2pwd");
+        $this->openAndWait('/admin');
+        $this->assertTextPresent('Licence types');
+        $this->assertTextPresent('OAK Link Information');
+        $this->assertTextNotPresent('Manage Documents');
+        $this->assertTextNotPresent('Review');
+        $this->assertTextNotPresent('Accounts');
+        $this->assertTextNotPresent('Access Control');
+        $this->assertTextNotPresent('Manage Collections');
+        $this->assertTextNotPresent('Manage Series');
+        $this->assertTextNotPresent('Languages');
+        $this->assertTextNotPresent('Publication statistics');
+        $this->assertTextNotPresent('Institution (dispersive body)');
+        $this->assertTextNotPresent('Manage Enrichmentkeys');
+        $this->assertTextNotPresent('System Information');
+    }
+    
+    /**
+     * Prüft, ob auf die Seite zur Verwaltung von Lizenzen zugegriffen werden kann.
+     */
+    public function testAccessLicenceController() {
+        $this->switchToEnglish();
+        $this->login("security2", "security2pwd");
+        $this->openAndWait('/admin/licence');
+        $this->assertElementContainsText('//html/head/title', 'Admin Licences');
+    }
+    
+    /**
+     * Prüft, das nicht auf die Seite zur Verwaltung von Dokumenten zugegriffen werden kann.
+     */
+    public function testNoAccessDocumentsController() {
+        $this->switchToEnglish();
+        $this->login("security2", "security2pwd");
+        $this->openAndWait('/admin/documents');
+        $this->assertElementNotContainsText('//html/body', 'You need another identity to access this page.');
+    }
+
+}
+
+?>
