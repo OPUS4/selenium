ad79d857d43d2a59dd2cd4437b29f0ee1d873d74
Jens Schwidder <schwidder@zib.de>
2012-12-12 15:11:38 +0000

    Dateiabschluß entfernt.
    git-svn-id: https://svn.zib.de/opus4dev/selenium@10985 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/authorisation/TestCaseAuthorisation.php
+++ tests/authorisation/TestCaseAuthorisation.php
@@ -25,8 +25,8 @@
  * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
- * @category    TODO
- * @package     TODO
+ * @category    Selenium Tests
+ * @package     Authorisation
  * @author      Edouard Simon (edouard.simon@zib.de)
  * @copyright   Copyright (c) 2008-2012, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
@@ -73,5 +73,3 @@ class TestCaseAuthorisation extends TestCase {
     }
     
 }
-
-?>
