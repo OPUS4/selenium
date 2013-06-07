3b9ea2a808072d7516f8dc7e1480cd0a62c80624
Sascha Szott <szott@gmx.de>
2013-06-07 09:28:59 +0000

    added regression test for OPUSVIER-2819
    git-svn-id: https://svn.zib.de/opus4dev/selenium@11329 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/publish/Opusvier2819Test.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    TODO
+ * @package     TODO
+ * @author      Sascha Szott <szott@zib.de>
+ * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+require_once 'TestCasePublish.php';
+
+class Opusvier2819Test extends TestCasePublish {
+
+    public function testValidationErrorMessageIsTranslated() {
+        $this->switchToGerman();
+        $this->goToSecondStep('demo');
+
+        // clear required field
+        $this->type('PersonSubmitterLastName_1', '');
+
+        $this->goToThirdStep();
+        $this->waitForPageToLoad();
+
+        $this->assertTextPresent('Es sind Fehler aufgetreten. Bitte beachten Sie die Fehlermeldungen an den Formularfeldern.');
+        $this->assertTextNotPresent('Value is required and can\'t be empty');
+
+        $this->type('PersonSubmitterLastName_1', 'Doe');
+
+        $this->goToThirdStep();
+        $this->waitForPageToLoad();
+
+        $this->assertTextPresent('Bitte überprüfen Sie Ihre Eingaben.');
+        $this->assertTextNotPresent('Es sind Fehler aufgetreten. Bitte beachten Sie die Fehlermeldungen an den Formularfeldern.');
+        $this->assertTextNotPresent('Value is required and can\'t be empty');
+
+        $this->click('abort');
+    }
+}
+
+?>
