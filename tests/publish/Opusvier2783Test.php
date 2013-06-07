92dc6a45f0830db8547badcdaec95ced07daf19f
Sascha Szott <szott@gmx.de>
2013-06-07 11:52:55 +0000

    added package annotation
    git-svn-id: https://svn.zib.de/opus4dev/selenium@11342 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/publish/Opusvier2783Test.php
+++ tests/publish/Opusvier2783Test.php
@@ -25,7 +25,7 @@
  * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *
  * @category    TODO
- * @package     TODO
+ * @package     Module_Publish
  * @author      Sascha Szott <szott@zib.de>
  * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
  * @license     http://www.gnu.org/licenses/gpl.html General Public License
