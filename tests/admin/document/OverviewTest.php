5ef49d0bed4f766fd6fcba49bc03e873c196b011
Jens Schwidder <schwidder@zib.de>
2013-06-14 14:51:48 +0000

    Selenium Test für Metadaten-Übersicht (auf Englisch) ergänzt. Personen und Collections fehlen noch mangels ausreichender IDs im HTML. Issue #OPUSVIER-2161 - Unit Test für die Anzeige eines vollständig besetzten Dokumentes
    git-svn-id: https://svn.zib.de/opus4dev/selenium@11489 0a49f362-4213-0410-86e3-e96cb6c4fbae

--- tests/admin/document/OverviewTest.php
+++ tests/admin/document/OverviewTest.php
@@ -69,9 +69,171 @@ class OverviewTest extends TestCase {
         
         $this->openAndWait('/admin/document/index/id/146');
         
+        // Information in Actionbox
+        $this->assertElementContainsText('//dd[@id="Document-ServerState"]', 'Published');
+        $this->assertElementContainsText('//dd[@id="Document-ServerDatePublished"]', '2012/01/03');
+        $this->assertElementContainsText('//dd[@id="Document-ServerDateModified"]', '2012/01/03');
+        
+        // General
+        $this->assertElementContainsText('//dd[@id="Document-General-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-General-Type"]', 'Master\'s Thesis');
+        $this->assertElementContainsText('//dd[@id="Document-General-PublishedDate"]', '2007/04/30');
+        $this->assertElementContainsText('//dd[@id="Document-General-PublishedYear"]', '2008');
+        $this->assertElementContainsText('//dd[@id="Document-General-CompletedDate"]', '2011/12/01');
+        $this->assertElementContainsText('//dd[@id="Document-General-CompletedYear"]', '2009');
+        
+        // Persons TODO
+
+        // Titles
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Main-TitleMain0-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Main-TitleMain0-Value"]', 'KOBV');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Main-TitleMain1-Language"]', 'English');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Main-TitleMain1-Value"]', 'COLN');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Additional-TitleAdditional0-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Additional-TitleAdditional0-Value"]', 
+                'Kooperativer Biblioheksverbund Berlin-Brandenburg');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Parent-TitleParent0-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Parent-TitleParent0-Value"]', 'Parent Title');
+
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Sub-TitleSub0-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Sub-TitleSub0-Value"]', 'Service-Zentrale');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Sub-TitleSub1-Language"]', 'English');
+        $this->assertElementContainsText('//dd[@id="Document-Titles-Sub-TitleSub1-Value"]', 'Service Center');
+        
+        // Bibliographische Informationen
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Edition"]', '1');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Volume"]', '2');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-PublisherName"]', 'Foo Publishing');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-PublisherPlace"]', 'Timbuktu');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-PageCount"]', '4');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-PageFirst"]', '1');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-PageLast"]', '4');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Issue"]', '3');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-ContributingCorporation"]', 'Baz University');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-CreatingCorporation"]', 'Bar University');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-ThesisDateAccepted"]', '2010/11/02');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-ThesisYearAccepted"]', '1999');
+        $this->assertElementContainsText('//dt[@id="Document-Bibliographic-BelongsToBibliography"]', 'Belongs to Bibliography');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Publishers-ThesisPublisher0-Institute"]', 'Foobar Universitätsbibliothek');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Publishers-ThesisPublisher1-Institute"]', 'Institute with DNB contact ID');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Grantors-ThesisGrantor0-Institute"]', 'Foobar Universität');
+        $this->assertElementContainsText('//dd[@id="Document-Bibliographic-Grantors-ThesisGrantor1-Institute"]', 'School of Life');
+        
+        // Series
+        $this->assertElementContainsText('//dd[@id="Document-Series-Series0-SeriesId"]', 'MySeries');
+        $this->assertElementContainsText('//dd[@id="Document-Series-Series0-Number"]', '5/5');
+        $this->assertElementContainsText('//dd[@id="Document-Series-Series0-SortOrder"]', '6');
+        
+        // Enrichments
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment0-KeyName"]', 'validtestkey');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment0-Value"]', 'Köln');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment1-KeyName"]', 'SourceSwb');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment1-Value"]', 'http://www.test.de');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment2-KeyName"]', 'SourceTitle');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment2-Value"]', 'Dieses Dokument ist auch erschienen als ...');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment3-KeyName"]', 'ClassRvk');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment3-Value"]', 'LI 99660');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment4-KeyName"]', 'ContributorsName');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment4-Value"]', 'John Doe (Foreword) and Jane Doe (Illustration)');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment5-KeyName"]', 'Event');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment5-Value"]', 'Opus4 OAI-Event');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment6-KeyName"]', 'City');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment6-Value"]', 'Opus4 OAI-City');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment7-KeyName"]', 'Country');
+        $this->assertElementContainsText('//dd[@id="Document-Enrichments-Enrichment7-Value"]', 'Opus4 OAI-Country');
+        
+        // Collections TODO
+        
+        // Abstracts
+        $this->assertElementContainsText('//dd[@id="Document-Content-Abstracts-TitleAbstract0-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-Content-Abstracts-TitleAbstract0-Value"]', 
+                'Die KOBV-Zentrale in Berlin-Dahlem.');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Content-Abstracts-TitleAbstract1-Language"]', 'English');
+        $this->assertElementContainsText('//dd[@id="Document-Content-Abstracts-TitleAbstract1-Value"]', 
+                'Lorem impsum.');
+        
+        // Subjects
+        $this->assertElementContainsText('//dd[@id="Document-Content-Subjects-Swd-Subject0-Value"]', 'Berlin');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Content-Subjects-Uncontrolled-Subject0-Language"]', 'German');
+        $this->assertElementContainsText('//dd[@id="Document-Content-Subjects-Uncontrolled-Subject0-Value"]', 'Palmöl');
+        
+        // Identifier
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier0-Type"]', 'old Identifier');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier0-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier1-Type"]', 'Serial Number');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier1-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier2-Type"]', 'Uuid');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier2-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier3-Type"]', 'ISBN');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier3-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier4-Type"]', 'URN');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier4-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier5-Type"]', 'DOI');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier5-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier6-Type"]', 'Handle');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier6-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier7-Type"]', 'URL');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier7-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier8-Type"]', 'ISSN');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier8-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier9-Type"]', 'STD-DOI');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier9-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier10-Type"]', 'CRIS-Link');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier10-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier11-Type"]', 'SplashURL');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier11-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier12-Type"]', 'Opus3 Id');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier12-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier13-Type"]', 'Opac Id');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier13-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier14-Type"]', 'Pubmed Id');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier14-Value"]', '123');
+        
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier15-Type"]', 'ArXiv Id');
+        $this->assertElementContainsText('//dd[@id="Document-Identifiers-Identifier15-Value"]', '123');
+        
         // Lizenzen (Name der Lizenz nicht übersetzt)
         $this->assertElementContainsText('//fieldset[@id="fieldset-Licences"]/legend', 'Licences');
         $this->assertElementContainsText('//dt[@id="Document-Licences-licence4"]', 'Creative Commons - Namensnennung'); 
+        
+        // Patents
+        $this->assertElementContainsText('//dd[@id="Document-Patents-Patent0-Number"]', '1234');
+        $this->assertElementContainsText('//dd[@id="Document-Patents-Patent0-Countries"]', 'DDR');
+        $this->assertElementContainsText('//dd[@id="Document-Patents-Patent0-YearApplied"]', '1970');
+        $this->assertElementContainsText('//dd[@id="Document-Patents-Patent0-Application"]', 'The foo machine.');
+        $this->assertElementContainsText('//dd[@id="Document-Patents-Patent0-DateGranted"]', '1970/01/01');
+
+        // Notes
+        $this->assertElementContainsText('//dt[@id="Document-Notes-Note0-Visibility"]', 'Public');
+        $this->assertElementContainsText('//dd[@id="Document-Notes-Note0-Message"]', 'Für die Öffentlichkeit');
+        $this->assertElementNotPresent('//dt[@id="Document-Notes-Note1-Visibility"]');
+        $this->assertElementContainsText('//dd[@id="Document-Notes-Note1-Message"]', 'Für den Admin');
     }
 
 }
