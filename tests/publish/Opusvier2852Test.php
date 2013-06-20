bc06e66d5140bdba82b118787f163f1496d936a9
Sascha Szott <szott@gmx.de>
2013-06-20 16:21:28 +0000

    added regression test for OPUSVIER-2852 Issue #OPUSVIER-2852 - Beim Abspeichern des Opus_Document im Formularschritt 3 (Deposit) erfolgt kein ExceptionHandling
    git-svn-id: https://svn.zib.de/opus4dev/selenium@11664 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/publish/Opusvier2852Test.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    TODO
+ * @package     Module_Publish
+ * @author      Sascha Szott <szott@zib.de>
+ * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+require_once 'TestCasePublish.php';
+
+class Opusvier2852Test extends TestCasePublish {
+
+    public function testDepositWithMissingEnrichmentKeyForDepositAction() {
+        $this->disableScreenshots();
+        
+        $this->goToSecondStep('demo');
+        $this->type('Enrichmentfoobarbaz_1', 'OPUSVIER2852Deposit');
+        $this->goToThirdStep();
+        $this->waitForPageToLoad();
+        
+        // open administration in another window        
+        $this->openWindow('/admin', 'admin-window');
+        $this->selectWindow('admin-window');
+
+        // delete enrichment key which is used in publish form
+        $this->login();
+        $this->open('/admin/enrichmentkey');
+        $this->waitForPageToLoad();
+        $this->assertTextPresent('foobarbaz');
+        
+        $this->open('/admin/enrichmentkey/delete/name/foobarbaz');
+        $this->waitForPageToLoad();        
+        
+        // try to submit document in first window: enrichment key does not exist anymore
+        $this->selectWindow(null);
+        $this->click('send');
+        $this->waitForPageToLoad();
+
+        $this->restoreEnrichmentKey();
+
+        $this->assertTextNotPresent('PDOException');
+        $this->assertTextNotPresent('Integrity constraint violation');
+
+        $this->enableScreenshots();
+    }
+
+    public function testDepositWithMissingEnrichmentKeyForCheckAction() {
+        $this->disableScreenshots();
+
+        $this->goToSecondStep('demo');
+        $this->type('Enrichmentfoobarbaz_1', 'OPUSVIER2852Check');
+
+        // open administration in another window
+        $this->openWindow('/admin', 'admin-window');
+        $this->selectWindow('admin-window');
+
+        // delete enrichment key which is used in publish form
+        $this->login();
+        $this->open('/admin/enrichmentkey');
+        $this->waitForPageToLoad();
+        $this->assertTextPresent('foobarbaz');
+
+        $this->open('/admin/enrichmentkey/delete/name/foobarbaz');
+        $this->waitForPageToLoad();
+
+        // try to submit document in first window: enrichment key does not exist anymore
+        $this->selectWindow(null);
+        $this->goToThirdStep();
+        $this->waitForPageToLoad();
+
+        $this->restoreEnrichmentKey();
+
+        $this->assertTextNotPresent('Publish_Model_FormIncorrectEnrichmentKeyException');
+
+        $this->enableScreenshots();
+    }
+
+    private function restoreEnrichmentKey() {
+        $this->selectWindow('admin-window');
+
+        $this->open('/admin/enrichmentkey');
+        $this->waitForPageToLoad();
+
+        $this->open('/admin/enrichmentkey/new');
+        $this->type('name', 'foobarbaz');
+        $this->click('submit');
+        $this->waitForPageToLoad();
+
+        $this->selectWindow(null);        
+    }
+    
+}
+
+?>
