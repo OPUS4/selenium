74f20810ae35389f0de879b78324db7def2e2abf
Jens Schwidder <schwidder@zib.de>
2013-09-17 11:17:45 +0000

    OPUSVIER-1335 - Selenium Test, der mit Hilfe von URNs eine Exception provoziert.
    git-svn-id: https://svn.zib.de/opus4dev/selenium@12672 0a49f362-4213-0410-86e3-e96cb6c4fbae

---
+++ tests/admin/document/ExceptionTest.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of OPUS. The software OPUS has been originally developed
+ * at the University of Stuttgart with funding from the German Research Net,
+ * the Federal Department of Higher Education and Research and the Ministry
+ * of Science, Research and the Arts of the State of Baden-Wuerttemberg.
+ *
+ * OPUS 4 is a complete rewrite of the original OPUS software and was developed
+ * by the Stuttgart University Library, the Library Service Center
+ * Baden-Wuerttemberg, the Cooperative Library Network Berlin-Brandenburg,
+ * the Saarland University and State Library, the Saxon State Library -
+ * Dresden State and University Library, the Bielefeld University Library and
+ * the University Library of Hamburg University of Technology with funding from
+ * the German Research Foundation and the European Regional Development Fund.
+ *
+ * LICENCE
+ * OPUS is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the Licence, or any later version.
+ * OPUS is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details. You should have received a copy of the GNU General Public License
+ * along with OPUS; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
+ * @category    Selenium Test
+ * @package     Module_Admin
+ * @author      Jens Schwidder <schwidder@zib.de>
+ * @copyright   Copyright (c) 2008-2013, OPUS 4 development team
+ * @license     http://www.gnu.org/licenses/gpl.html General Public License
+ * @version     $Id$
+ */
+
+require_once 'TestCase.php';
+
+class ExceptionTest extends TestCase {
+
+    public function testMessageForExceptionStoringDocument() {
+        $this->switchToEnglish();
+        $this->login();
+
+        $this->openAndWait('/admin/document/edit/id/250');
+
+        $this->assertElementNotPresent('Document-Identifiers-Identifier0-Type');
+
+        $this->clickAndWait('Document-Identifiers-Add');
+
+        $this->assertElementPresent('Document-Identifiers-Identifier0-Type');
+        $this->assertElementNotPresent('Document-Identifiers-Identifier1-Type');
+
+        $this->clickAndWait('Document-Identifiers-Add');
+
+        $this->assertElementPresent('Document-Identifiers-Identifier0-Type');
+        $this->assertElementPresent('Document-Identifiers-Identifier1-Type');
+
+        $this->select('Document-Identifiers-Identifier0-Type', 'URN');
+        $this->select('Document-Identifiers-Identifier1-Type', 'URN');
+
+        $this->type('Document-Identifiers-Identifier0-Value', 'TestURN');
+        $this->type('Document-Identifiers-Identifier1-Value', 'TestURN');
+
+        $this->clickAndWait('Document-ActionBox-Save');
+
+        $xpath = '//*[@id="actionboxContainer"]/*[@class="messagesContainer"]/*[@class="messages"]';
+
+        $this->assertElementPresent($xpath);
+        $this->assertElementContainsText($xpath, 'unexpected exception occurred while saving the document');
+        $this->assertElementContainsText($xpath, 'urn collision (documents 250)');
+    }
+
+}
